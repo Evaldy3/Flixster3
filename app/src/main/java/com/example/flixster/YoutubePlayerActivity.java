c7e7c38697f52c50e308b67c913b5ef714652545
Evaldy3 <evaldyaristil@gmail.com>
2020-07-18 01:14:35 -0400

    fix missing files

---
+++ app/src/main/java/com/example/flixster/YoutubePlayerActivity.java
@@ -0,0 +1,40 @@
+package com.example.flixster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+public class YoutubePlayerActivity extends AppCompatActivity {
+
+    private static final String YOUTUBE_API_KEY = "AIzaSyAF2rY5lepnmTpBZMICbu0p3zod6f08R4U";
+    YouTubePlayerFragment youtubeFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_player);
+
+        youtubeFragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtubeFragment);
+        String key = getIntent().getStringExtra("key");
+        initYoutube(key);
+
+    }
+
+    private void initYoutube(final String key) {
+        youtubeFragment.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo(key);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                finish();
+            }
+        });
+    }
+}
